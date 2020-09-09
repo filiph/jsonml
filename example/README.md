30fd3034a8c7d6bb34b285c0bcdfd5d115174596
Filip Hracek <filip.hracek@gmail.com>
2020-09-09 16:38:42 -0700

    Add README to example/

---
+++ example/README.md
@@ -0,0 +1,7 @@
+This directory includes two examples:
+
+* `domtest.dart` takes an HTML string, parses it to JSONML, then
+  renders the JSONML to the web page.
+* `svgtest.dart` does the same with an SVG string.
+
+Both of these only work on the web.
--- example/svgtest.dart
+++ example/svgtest.dart
@@ -4,7 +4,6 @@ import "package:jsonml/html2jsonml.dart";
 
 void main() {
   var destination = querySelector("div#destination");
-//  var jsonml = encode("<p id='main'><a href='http://www.example.com/'>This</a> is <em><strong>very</strong></em> interesting.</p>");
   var jsonml = encodeToJsonML(
       """<svg width="100" height="100"> <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /> </svg>""");
   destination.append(decodeToDom(jsonml, unsafe: true));
